8a497f68633d018a018bc935fcea801fe603b937
106504252 <106504252@student.swin.edu.au>
2026-05-07 18:58:56 +1000

    lab09part2work

---
+++ search_form.php
@@ -0,0 +1,9 @@
+<php>
+    <form method="post" action="search_result.php">
+        <label for="searchfield">Search:</label>
+        <input type="text" name="searchfield" required><br>
+
+        <input type="submit" value="">
+    </form>
+</php>
+    
